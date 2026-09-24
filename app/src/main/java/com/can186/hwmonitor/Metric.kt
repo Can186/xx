9a18753dfffd7e9c9a746bf9c174312f089218cf
Can186 <340498650@qq.com>
2026-09-25 07:05:57 +0800

    Add MetricType enum and Metric data class
    Define MetricType enum and Metric data class.

---
+++ app/src/main/java/com/can186/hwmonitor/Metric.kt
@@ -0,0 +1,22 @@
+package com.can186.hwmonitor
+
+enum class MetricType {
+    PERCENT,
+    FREQ_MHZ,
+    FREQ_KHZ,
+    TEMP_MC,
+    TEMP_C,
+    BYTES_MB,
+    RAW
+}
+
+data class Metric(
+    val id: String,
+    val name: String,
+    val path: String,
+    val type: MetricType,
+    var value: String = "--",
+    var enabled: Boolean = true
+)
+
+enum class ReadMode { BASIC, ROOT, SHIZUKU }
