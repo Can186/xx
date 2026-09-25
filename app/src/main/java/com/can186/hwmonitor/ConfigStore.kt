57e5cfdfe0fa3bae5dbff709c2768911440d2c12
Can186 <340498650@qq.com>
2026-09-25 10:20:48 +0800

    Update ConfigStore.kt

--- app/src/main/java/com/can186/hwmonitor/ConfigStore.kt
+++ app/src/main/java/com/can186/hwmonitor/ConfigStore.kt
@@ -95,7 +95,7 @@ object ConfigStore {
             .getInt(KEY_TEXT_COLOR, 0xFFFFFFFF.toInt())
 
     fun loadOverlayPosition(ctx: Context): OverlayPosition {
-        // ✅ 关键修复点：这一行必须是 "val s ="，不能是 "val ="
+        // ✅ 注意看这里，必须是 "val s ="，不能写成 "val ="
         val  = ctx.getSharedPreferences(PREF, Context.MODE_PRIVATE)
             .getString(KEY_POSITION, OverlayPosition.TOP_LEFT.name)
         return try {
